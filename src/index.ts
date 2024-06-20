82b859ed381376c8eddba45718172604968d149a
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-06-21 01:45:12 +0900

    refactor: クラスで実装するよりも単体の関数として実装する方がシンプルでわかりやすいので方針変更

--- src/index.ts
+++ src/index.ts
@@ -37,114 +37,52 @@ export interface LimitFunction extends LimitFunctionBase {
     clearQueue(): void;
 }
 
-type TypedPropertyDescriptors<T extends object> = {
-    [Key in keyof T]: TypedPropertyDescriptor<T[Key]>;
-};
+/**
+ * 並列実行するタスクを指定した値で制限する`limit`関数を生成します。
+ * @param concurrencySpec 並列実行する最大数を指定します。
+ *
+ * 1以上の数値を指定できます。
+ * @returns 生成した`limit`関数を返します。
+ * @throws concurrencyに不正な値(数値以外や1未満の数値)を指定したときに例外を投げます。
+ */
+export default function pLimit(concurrencySpec: number): LimitFunction {
+    validation(concurrencySpec);
 
-class LimitExecutor {
+    /** 並列実行する最大数 */
+    let concurrency = concurrencySpec;
     /** 実行中のタスクの数 */
-    private activeCount = 0;
+    let activeCount = 0;
     /** 待機中のタスク */
-    private readonly queue = Queue<() => void>();
+    const queue = Queue<() => void>();
     /** 即時実行するためのPromiseチェーン */
-    private starter = Promise.resolve();
+    let starter = Promise.resolve();
 
-    /**
-     * Creates an instance of LimitExecutor.
-     * @param concurrency 同時実行数の最大値
-     * @memberof LimitExecutor
-     */
-    private constructor(private concurrency: number) {}
-
-    /**
-     * このExecutorで同時に実行できる最大数をconcurrencyに制限してタスクを非同期に実行します。
-     * @private
-     * @param task 実行するタスク。
-     * @returns タスクの実行結果
-     * @memberof LimitExecutor
-     */
-    private async exec<R>(task: () => R): Promise<Awaited<R>> {
-        // 待機状態で開始
-        await this.pending();
-        ++this.activeCount;
-        try {
-            // eslint-disable-next-line @typescript-eslint/await-thenable -- taskは基本的にPromiseを返す
-            return await task();
-        } finally {
-            --this.activeCount;
-            if (this.activeCount < this.concurrency) {
-                // activeCountが減って余裕ができれば、次のタスクの待機を解除する
-                this.resumeNext();
-            }
-        }
-    }
-
-    /**
-     * 待機状態のPromiseを返します。
-     * @private
-     * @returns 待機状態のPromise
-     * @memberof LimitExecutor
-     */
-    private pending() {
-        try {
-            // 待機状態にします
-            return new Promise<void>(this.queue.enqueue);
-        } finally {
-            // と同時に待機解除を予約します。
-            this.starter = this.starter.then(() => {
-                if (this.activeCount < this.concurrency) {
-                    // 余裕があれば開始
-                    this.resumeNext();
+    return Object.defineProperties(
+        (async (task, ...parameters) => {
+            // 待機状態で開始
+            await pending();
+            ++activeCount;
+            try {
+                // eslint-disable-next-line @typescript-eslint/await-thenable -- taskは通常Promiseを返すので問題ない
+                return await task(...parameters);
+            } finally {
+                --activeCount;
+                if (activeCount < concurrency) {
+                    // activeCountが減って余裕ができれば、次のタスクの待機を解除する
+                    resumeNext();
                 }
-            });
-        }
-    }
-
-    /**
-     * pendingで生成されたPromiseの待機状態を解除します。
-     * @private
-     * @memberof LimitExecutor
-     */
-    private resumeNext() {
-        this.queue.dequeue()?.();
-    }
-
-    /**
-     * 同時実行数の最大値を設定します。
-     *
-     * より大きい数値に変更すると、待機中のタスクから追加で実行開始します。
-     *
-     * より小さい数値に変更した場合は何もしません。
-     * @param newConcurrency 同時実行数の最大値。
-     */
-    private setConcurrency(newConcurrency: number) {
-        LimitExecutor.validation(newConcurrency);
-        this.concurrency = newConcurrency;
-        let count = this.concurrency - this.activeCount;
-        while (count-- > 0) {
-            this.resumeNext();
-        }
-    }
-
-    /**
-     * LimitFunctionのプロパティを定義するPropertyDescriptorを返します。
-     * @readonly
-     * @private
-     * @type {TypedPropertyDescriptors<LimitFunction>}
-     * @memberof LimitExecutor
-     */
-    private get descriptors(): TypedPropertyDescriptors<LimitFunction> {
-        // 最初に1回使用するだけなので値そのものではなくgetterで実装しています。
-        return {
-            activeCount: { get: () => this.activeCount },
-            pendingCount: { get: () => this.queue.size },
+            }
+        }) as LimitFunctionBase,
+        {
+            activeCount: { get: () => activeCount },
+            pendingCount: { get: () => queue.size },
+            clearQueue: { value: queue.clear },
             concurrency: {
-                get: () => this.concurrency,
-                set: this.setConcurrency.bind(this),
+                get: () => concurrency,
+                set: setConcurrency,
             },
-            clearQueue: { value: this.queue.clear },
-        };
-    }
+        } satisfies TypedPropertyDescriptors<LimitFunction>,
+    );
 
     /**
      * 外部に公開するAPIの引数が適切なものかチェックして、不適切であれば例外を投げます。
@@ -153,7 +91,7 @@ class LimitExecutor {
      * @memberof LimitExecutor
      * @throws concurrencyに不正な値(数値以外や1未満の数値)を指定したときに例外を投げます。
      */
-    private static validation(concurrency: number) {
+    function validation(concurrency: number) {
         // 引数のチェック
         if (typeof concurrency === 'number' && concurrency >= 1) {
             return;
@@ -162,32 +100,45 @@ class LimitExecutor {
             'Expected `concurrency` to be a number from 1 and up',
         );
     }
-
     /**
-     * 並列実行するタスクを指定した値で制限する`limit`関数を生成します。
-     * @param concurrency 並列実行する最大数を指定します。
-     *
-     * 1以上の数値を指定できます。
-     * @returns 生成した`limit`関数を返します。
-     * @throws concurrencyに不正な値(数値以外や1未満の数値)を指定したときに例外を投げます。
+     * pendingで生成されたPromiseの待機状態を解除します。
+     * @private
+     * @memberof LimitExecutor
      */
-    static generate(concurrency: number): LimitFunction {
-        LimitExecutor.validation(concurrency);
-        const executor = new LimitExecutor(concurrency);
-        const limit: LimitFunctionBase = (task, ...parameters) =>
-            executor.exec(() => task(...parameters));
-        return Object.defineProperties(limit, executor.descriptors);
+    function resumeNext() {
+        queue.dequeue()?.();
     }
-}
-
-/**
- * 並列実行するタスクを指定した値で制限する`limit`関数を生成します。
- * @param concurrency 並列実行する最大数を指定します。
- *
- * 1以上の数値を指定できます。
- * @returns 生成した`limit`関数を返します。
- * @throws concurrencyに不正な値(数値以外や1未満の数値)を指定したときに例外を投げます。
- */
-export default function pLimit(concurrency: number): LimitFunction {
-    return LimitExecutor.generate(concurrency);
+    /**
+     * 待機状態のPromiseを返します。
+     * @private
+     * @returns 待機状態のPromise
+     * @memberof LimitExecutor
+     */
+    function pending() {
+        // 余裕があれば待機を解除する処理を予約しておきます。
+        starter = starter.then(() => {
+            if (activeCount < concurrency) {
+                resumeNext();
+            }
+        });
+        // 待機状態にします
+        return new Promise<void>(queue.enqueue);
+    }
+    /**
+     * 同時実行数の最大値を設定します。
+     *
+     * より大きい数値に変更すると、待機中のタスクから追加で実行開始します。
+     *
+     * より小さい数値に変更した場合は何もしません。
+     * @param newConcurrency 同時実行数の最大値。
+     */
+    function setConcurrency(newConcurrency: number) {
+        validation(newConcurrency);
+        concurrency = newConcurrency;
+        // 増えた分だけ待機解除
+        let count = concurrency - activeCount;
+        while (count-- > 0) {
+            resumeNext();
+        }
+    }
 }
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,8 @@
 declare global {
+    type TypedPropertyDescriptors<T extends object> = {
+        readonly [K in keyof T]: TypedPropertyDescriptor<T[K]>;
+    };
+
     interface ObjectConstructor {
         defineProperties<T extends object, Descriptors extends object>(
             o: T,
