255f01d9021b09daef84e51712aa5c3d3948420b
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-07-04 23:59:02 +0900

    refactor: terminal.nextがundefinedならキューが空とみなして最適化

--- src/Queue.ts
+++ src/Queue.ts
@@ -28,45 +28,56 @@ export function Queue<T>(): Queue<T> {
         value: T;
         next?: Node;
     }
+    // terminal.nextはキューの先頭(headなどにしなかったのはnextで揃えることでenqueueで楽をするため)
+    type Terminal =
+        // nextとtailのどちらもが存在しているか
+        | { next: Node; tail: Node }
+        // どちらもが存在していないかの二択
+        | { next?: undefined; tail?: undefined };
 
-    const terminal: {
-        next?: Node;
-        tail?: Node;
-    } = {};
+    const terminal: Terminal = {};
     let size = 0;
 
-    const clear = () => {
-        delete terminal.next;
-        delete terminal.tail;
+    function clear() {
+        // クリアするのはnextだけでよい
+        terminal.next = undefined;
         size = 0;
-    };
+    }
+
+    function enqueue(value: T) {
+        const node: Node = { value };
+        // キューに存在していれば末尾の次に、空ならば先頭に追加
+        (terminal.next ? terminal.tail : terminal).next = node;
+        // 末尾のNodeを記憶しておく
+        terminal.tail = node;
+        ++size;
+    }
+
+    function dequeue() {
+        if (!terminal.next) {
+            // キューが空ならundefinedを返す。
+            return undefined;
+        }
+        const { next, value } = terminal.next;
+        // 先頭を次のNodeに差し替え(↑のif文でnextにundefinedが代入できなくなっているので型アサーションを追加)
+        (terminal as Terminal).next = next;
+        --size;
+        return value;
+    }
+
+    function* iterator() {
+        for (let node = terminal.next; node; node = node.next) {
+            yield node.value;
+        }
+    }
 
     return {
         get size(): number {
             return size;
         },
-        enqueue(value) {
-            terminal.tail = (terminal.tail ?? terminal).next = { value };
-            ++size;
-        },
-        dequeue() {
-            const head = terminal.next;
-            if (!head) {
-                return undefined;
-            }
-            if (head.next) {
-                terminal.next = head.next;
-                --size;
-            } else {
-                clear();
-            }
-            return head.value;
-        },
+        enqueue,
+        dequeue,
         clear,
-        *[Symbol.iterator]() {
-            for (let node = terminal.next; node; node = node.next) {
-                yield node.value;
-            }
-        },
+        [Symbol.iterator]: iterator,
     };
 }
