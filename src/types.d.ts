5efe1b4192d1cd78cb473d11612ee9194c1388ae
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-07-25 02:13:42 +0900

    refactor: 不要なプロパティを指定したときにエラー指摘箇所がそのプロパティの位置になるように変更

--- src/types.d.ts
+++ src/types.d.ts
@@ -41,6 +41,11 @@ type IsReadonlyProperty<
     Key extends keyof Target,
 > = Equal<Pick<Target, Key>, Readonly<Pick<Target, Key>>>;
 
+/**
+ * アクセサー(setter/getter)を持つプロパティのためのプロパティ記述子です。
+ * @template T プロパティの型
+ * @template Readonly 参照専用のプロパティの場合にはtrueを指定します。
+ */
 type AccessorPropertyDescriptor<
     T = void,
     Readonly extends boolean = never,
@@ -50,23 +55,24 @@ type AccessorPropertyDescriptor<
     Omit<PropertyDescriptor, 'get' | 'set' | 'value' | 'writable'> & {
         // getが未指定でもエラーにはならないが、レアケースのため考慮に入れない。
         get: Getter;
-        value?: never;
-        writable?: never;
     } & (true extends Readonly
-            ? { set?: never }
+            ? unknown
             : false extends Readonly
               ? { set: Setter }
               : { set?: Setter }),
     never
 >;
+/**
+ * データを保持するプロパティのためのプロパティ記述子です。
+ * @template T プロパティの型
+ * @template Readonly 参照専用のプロパティの場合にはtrueを指定します。
+ */
 type DataPropertyDescriptor<
     T = void,
     Readonly extends boolean = never,
     Value = IsVoid<T> extends true ? unknown : T,
 > = Omit<
     Omit<PropertyDescriptor, 'get' | 'set' | 'value' | 'writable'> & {
-        get?: never;
-        set?: never;
         // valueの指定がなくてもエラーにはならないが、型が確定できなくなるので考慮に入れない。
         value: Value;
     } & (true extends Readonly
@@ -78,11 +84,47 @@ type DataPropertyDescriptor<
 >;
 
 /**
- * 指定の型のプロパティを実装するための厳密なPropertyDescriptor
+ * Tに指定したinterfaceが持つプロパティのうち不要なものを指定できないようにします。
+ * @example
+ * interface A {
+ *     a1: string;
+ *     a2: number;
+ * }
+ * interface B {
+ *     b1: number;
+ *     b2: string;
+ * }
+ *
+ * type AB = A | B | PreventUnnecessaryPropertiesOf<A & B>;
+ *
+ * const a: AB = {
+ *     a1: 'abc',
+ *     a2: 123,
+ *     b1: 345,
+ * //  ^^ オブジェクト リテラルは既知のプロパティのみ指定できます。'b1' は型 'A' に存在しません。
+ * };
+ *
+ * type LooseAB = A | B;
+ * const a2: LooseAB = {
+ *     a1: 'abc',
+ *     a2: 123,
+ *     b1: 345, // no error
+ * };
+ */
+type PreventUnnecessaryPropertiesOf<T> = {
+    [K in keyof T]: Record<K, never> &
+        Partial<Record<Exclude<keyof T, K>, never>>;
+}[keyof T];
+
+/**
+ * 指定の型のプロパティを実装するための厳密なプロパティ記述子です。
+ * @template T プロパティの型
+ * @template Readonly 参照専用のプロパティの場合にはtrueを指定します。
  */
 type StrictPropertyDescriptor<T = void, Readonly extends boolean = never> =
     | AccessorPropertyDescriptor<T, Readonly>
-    | DataPropertyDescriptor<T, Readonly>;
+    | DataPropertyDescriptor<T, Readonly>
+    | PreventUnnecessaryPropertiesOf<PropertyDescriptor>;
 
 /**
  * 適切なPropertyDescriptorかどうかを判別する。
