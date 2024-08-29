56fafcf0591b98e84d82b7078016acc17eb58c80
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-08-29 22:07:16 +0900

    test: 定数や同じ処理をテストケース外に移動

--- tests/sample.test.ts
+++ tests/sample.test.ts
@@ -6,10 +6,87 @@ import {
 import { mkdir, rm, writeFile } from 'fs/promises';
 import { basename, resolve } from 'path';
 
+const sampleSource = /* ts */ `
+import pLimit from '@sugoroku-y/p-limit';
+
+const concurrency = 30;
+const count = 1000;
+const limit = pLimit(concurrency);
+let received = '';
+
+void Promise.all(
+    Array.from({ length: count }, (_, i) =>
+        limit(async (id: number) => {
+            received += id + 's';
+            await new Promise((r) => setTimeout(r, 0));
+            received += id + 'e';
+        }, i),
+    ),
+).then(() => {
+    function* makeExpected(concurrency: number, count: number) {
+        const numbers = Array.from({length:count}, (_,i) => i);
+        const start = numbers[Symbol.iterator]();
+        const end = numbers[Symbol.iterator]();
+        for (let r = concurrency; r-- >0;) {
+            const {value:s} =  start.next();
+            yield s + 's';
+        }
+        for (const e of end) {
+            yield e + 'e';
+            const {value: s} =  start.next();
+            if (s !== undefined) yield s + 's';
+        }
+    }
+    
+    const expected = ''.concat(...makeExpected(concurrency, count));
+    if (received === expected) {
+        console.log('success');
+        process.exit(0);
+    }
+    console.log('received:', received);
+    console.log('expected:', expected);
+    process.exit(1);
+});
+`;
+
+const table = {
+    esm: {
+        type: 'module',
+        tsConfig: {
+            compilerOptions: {
+                target: 'ES2018',
+                module: 'NodeNext',
+                moduleResolution: 'NodeNext',
+            },
+            'ts-node': { transpileOnly: true },
+        },
+        execCommand: [
+            'node',
+            '--import',
+            `"data:text/javascript,${
+                /* js */ `
+                import { register } from 'node:module';
+                import { pathToFileURL } from 'node:url';
+                register('ts-node/esm', pathToFileURL('./'));
+                `.replaceAll(/\s+/g, '')
+            }"`,
+            'index.ts',
+        ],
+    },
+    cjs: {
+        type: 'commonjs',
+        tsConfig: { compilerOptions: { target: 'ES2018' } },
+        execCommand: ['npx', 'ts-node', 'index.ts'],
+    },
+} as const;
+
+const npmInstall = ['npm', 'i', '../../..', 'ts-node', '--install-links'];
+
 describe('sample', () => {
-    test.performance.each(['esm', 'cjs'])(
+    test.performance.each(['esm', 'cjs'] as const)(
         'sample %s',
         async (dir) => {
+            const { type, tsConfig, execCommand } = table[dir];
             const options = {
                 shell: true,
                 cwd: resolve(__dirname, 'sample', dir),
@@ -17,119 +94,34 @@ describe('sample', () => {
             } satisfies SpawnSyncOptionsWithStringEncoding;
             await rm(options.cwd, { recursive: true, force: true });
             await mkdir(options.cwd, { recursive: true });
-            const pkg = {
+            await writeJson(resolve(options.cwd, 'package.json'), {
                 private: true,
-                type: dir === 'cjs' ? 'commonjs' : 'module',
-            };
-            await writeFile(
-                resolve(options.cwd, 'package.json'),
-                JSON.stringify(pkg, undefined, 4),
-                'utf8',
-            );
-            const tsConfig = {
-                compilerOptions: {
-                    target: 'ES2018',
-                    ...(dir === 'esm' && {
-                        module: 'NodeNext',
-                        moduleResolution: 'NodeNext',
-                    }),
-                },
-                ...(dir === 'esm' && {
-                    'ts-node': {
-                        transpileOnly: true,
-                    },
-                }),
-            };
-            await writeFile(
-                resolve(options.cwd, 'tsconfig.json'),
-                JSON.stringify(tsConfig, undefined, 4),
-                'utf8',
-            );
+                type,
+            });
+            await writeJson(resolve(options.cwd, 'tsconfig.json'), tsConfig);
             await writeFile(
                 resolve(options.cwd, 'index.ts'),
-                /* ts */ `
-                import pLimit from '@sugoroku-y/p-limit';
-                
-                const concurrency = 30;
-                const count = 1000;
-                const limit = pLimit(concurrency);
-                let received = '';
-                
-                void Promise.all(
-                    Array.from({ length: count }, (_, i) =>
-                        limit(async (id: number) => {
-                            received += id + 's';
-                            await new Promise((r) => setTimeout(r, 0));
-                            received += id + 'e';
-                        }, i),
-                    ),
-                ).then(() => {
-                    function* makeExpected(concurrency: number, count: number) {
-                        const numbers = Array.from({length:count}, (_,i) => i);
-                        const start = numbers[Symbol.iterator]();
-                        const end = numbers[Symbol.iterator]();
-                        for (let r = concurrency; r-- >0;) {
-                            const {value:s} =  start.next();
-                            yield s + 's';
-                        }
-                        for (const e of end) {
-                            yield e + 'e';
-                            const {value: s} =  start.next();
-                            if (s !== undefined) yield s + 's';
-                        }
-                    }
-                    
-                    const expected = ''.concat(...makeExpected(concurrency, count));
-                    if (received === expected) {
-                        console.log('success');
-                        process.exit(0);
-                    }
-                    console.log('received:', received);
-                    console.log('expected:', expected);
-                    process.exit(1);
-                });
-                `,
+                sampleSource,
                 'utf8',
             );
-            await expect(
-                spawnPromises(
-                    options,
-                    'npm',
-                    'i',
-                    '../../..',
-                    'ts-node',
-                    '--install-links',
-                ),
-            ).resolves.toBe(0);
-            await expect(
-                spawnPromises(
-                    options,
-                    ...(dir === 'cjs'
-                        ? ['npx', 'ts-node', 'index.ts']
-                        : [
-                              'node',
-                              '--import',
-                              /* js */ `"data:text/javascript,
-                                import { register } from 'node:module';
-                                import { pathToFileURL } from 'node:url';
-                                register('ts-node/esm', pathToFileURL('./'));
-                                "`.replaceAll(/\s+/g, ''),
-                              'index.ts',
-                          ]),
-                ),
-            ).resolves.toBe(0);
+            expect(await spawnPromises(options, ...npmInstall)).toBe(0);
+            expect(await spawnPromises(options, ...execCommand)).toBe(0);
         },
         60000,
     );
 });
 
+function writeJson(path: string, json: unknown) {
+    return writeFile(path, JSON.stringify(json, undefined, 4), 'utf8');
+}
+
 function spawnPromises(
     options: SpawnOptionsWithoutStdio,
     ...[command, ...args]: string[]
 ) {
-    process.stdout.write([command, ...args].join(' ').concat('\n'));
     const headings =
         typeof options.cwd === 'string' ? `${basename(options.cwd)}: ` : '';
+    process.stdout.write(headings + [command, ...args].join(' ').concat('\n'));
     const npm = spawn(command, args, options);
     return new Promise<number>((resolve, reject) => {
         npm.once('exit', (status) =>
